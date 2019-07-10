4ab345d78be5c8a06e98ce88505c39747d050d66
minoru <minoru.2021@icloud.com>
2019-07-10 10:26:33 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # post/<int:pk>/ = URLパターンを指定しています。
+    # <int:pk>Djangoは整数の値を期待し、その値がpkという名前の変数でビューに渡されることを意味しています。
+
 ]
